f41495d1948db9af70bd86b4a603503474d1d21b
DanThem2025 <daniel@them.com.au>
2025-07-11 13:47:16 +0930

    Update Container.php

--- Container.php
+++ Container.php
@@ -6,4 +6,8 @@ use Illuminate\Container\Container as BaseContainer;
 
 class Container extends BaseContainer
 {
+     public function terminating(Closure $callback)
+  {
+    add_action('shutdown', $callback);
+  }
 }
